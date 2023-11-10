f6fd0c5b86256088b14f299599bb2fa93ef8fd5c
Edvan Sabino <edvansabino@gmail.com>
2023-11-10 17:24:07 -0300

    Configurando a estrutura do projeto

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-alert("Olá, Mundo!");
+function calcularLevel() {
+  let heroName = document.querySelector("#hero-name");
+  let levelHero = document.querySelector("#hero-level");
+  let newLevelHero = Number(levelHero.value);
+  let resultName = document.querySelector("div#resultN");
+  let resultLevel = document.querySelector("div#resultL");
+  resultName.innerHTML = `O nome do seu herói é <strong>${heroName.value}</strong>`;
+  resultLevel.innerHTML = `O nível de XP do seu herói é <strong>${newLevelHero}</strong>`;
+}
