81011737864eb2f18f1884042b92ae1fe61f81bb
Edvan Sabino <edvansabino@gmail.com>
2023-11-11 17:26:58 -0300

    Iniciando a implementação da interatividade JS

--- scripts.js
+++ scripts.js
@@ -4,6 +4,26 @@ function calcularLevel() {
   let newLevelHero = Number(levelHero.value);
   let resultName = document.querySelector("div#resultN");
   let resultLevel = document.querySelector("div#resultL");
-  resultName.innerHTML = `O nome do seu herói é <strong>${heroName.value}</strong>`;
-  resultLevel.innerHTML = `O nível de XP do seu herói é <strong>${newLevelHero}</strong>`;
+  let resLevelHero = "";
+  //!resultLevel.innerHTML = `O nível de XP do seu herói é <strong>${newLevelHero}</strong>`;
+  if (newLevelHero <= 1000) {
+    resLevelHero = "Ferro";
+  } else if (newLevelHero > 1000 && newLevelHero <= 2000) {
+    resLevelHero = "Bronze";
+  } else if (newLevelHero > 2000 && newLevelHero <= 5000) {
+    resLevelHero = "Prata";
+  } else if (newLevelHero > 5000 && newLevelHero <= 7000) {
+    resLevelHero = "Ouro";
+  } else if (newLevelHero > 7000 && newLevelHero <= 8000) {
+    resLevelHero = "Platina";
+  } else if (newLevelHero > 8000 && newLevelHero <= 9000) {
+    resLevelHero = "Ascendente";
+  } else if (newLevelHero > 9000 && newLevelHero <= 10000) {
+    resLevelHero = "Imortal";
+  } else if (newLevelHero > 10000) {
+    resLevelHero = "Radiante";
+  } else {
+    resLevelHero = "[ERRO] Verifique o valor da XP informado!";
+  }
+  resultName.innerHTML = `O herói de nome <strong>${heroName.value}</strong> está no nível <strong>${resLevelHero}</strong>`;
 }
